8caef4926ff081377007ceddf08440377654345d
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-20 20:59:31 -0300

    setup :sparkles: foi iniciado o projeto.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //arquivos que vão utilizar o tailwindcss
+  content: [
+    './src/**/*tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
